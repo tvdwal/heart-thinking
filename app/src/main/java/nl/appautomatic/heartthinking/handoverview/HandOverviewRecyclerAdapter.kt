24df94ae38e39b11106777672034779eadd25735
Tim van de Wal <timvandewal@gmail.com>
2018-04-16 10:48:36 +0200

    Added onclicklistener

--- app/src/main/java/nl/appautomatic/heartthinking/handoverview/HandOverviewRecyclerAdapter.kt
+++ app/src/main/java/nl/appautomatic/heartthinking/handoverview/HandOverviewRecyclerAdapter.kt
@@ -1,12 +1,14 @@
 package nl.appautomatic.heartthinking.handoverview
 
 import android.content.Context
+import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import nl.appautomatic.heartthinking.R
 import nl.appautomatic.heartthinking.databinding.LayoutHandOverviewRecyclerItemBinding
+import nl.appautomatic.heartthinking.handviewer.HandViewerActivity
 import nl.appautomatic.heartthinking.poker.Hand
 
 class HandOverviewRecyclerAdapter(val viewModel: HandOverviewViewModel, val context: Context) : RecyclerView.Adapter<HandOverviewHolder>() {
@@ -14,6 +16,11 @@ class HandOverviewRecyclerAdapter(val viewModel: HandOverviewViewModel, val cont
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HandOverviewHolder {
         val inflater = LayoutInflater.from(parent.context)
         var binding = DataBindingUtil.inflate<LayoutHandOverviewRecyclerItemBinding>(inflater, R.layout.layout_hand_overview_recycler_item, parent, false)
+        binding.root.setOnClickListener {
+            val i = Intent(context, HandViewerActivity::class.java)
+            context.startActivity(i)
+
+        }
         return HandOverviewHolder(binding)
     }
 
